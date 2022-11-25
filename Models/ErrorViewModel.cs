15aa507ea2ae1b8f5b81b23f6cedf03115035843
Ashish Jain <arjain18@gmail.com>
2022-11-25 09:22:56 +0530

    started with basic template for project structure. provides docker support

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UrlShortner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
